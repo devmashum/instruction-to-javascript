a06a8051503d1dd25c62af1cb9de1f14f4e95d12
devmashum <dev.mashum@gmail.com>
2023-07-29 00:42:04 +0200

    new commit

---
+++ operation.js
@@ -0,0 +1,7 @@
+var onionPrice = 20;
+var eggPrice = 10;
+// console.log(onionPrice);
+// console.log(onionPrice + eggPrice);
+
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
---
+++ shorthand.js
@@ -0,0 +1,10 @@
+var onionPrice = 10;
+var orangePrice = 50;
+
+var totalPrice = onionPrice + orangePrice;
+
+// console.log(totalPrice);
+
+orangePrice -= 10;
+
+console.log(orangePrice);

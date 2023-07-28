f6472863efa5c37cc1d1bb509229cda8ddc75b59
devmashum <dev.mashum@gmail.com>
2023-07-29 01:39:34 +0200

    new commit

---
+++ conversion.js
@@ -0,0 +1,15 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2;
+
+
+var firstName = 'Faisal'
+var lastName = 'Mashum'
+
+var fullName = firstName + lastName;
+
+// console.log(fullName);
+
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);

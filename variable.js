ec3626faa6f176263c7e39129168b0e2fbb2e98a
devmashum <dev.mashum@gmail.com>
2023-07-29 18:40:45 +0200

    new commit

--- variable.js
+++ variable.js
@@ -1,3 +1,19 @@
-var price = 11;
-var age = 30;
-var temperature = 38;
+// Number type variable
+
+var sunglassPrice = 120;
+
+// String type variable
+
+var specialOneName = "Ariana Grande";
+
+//  Boolean type variable
+
+Var isSingle = true;
+
+// tow types of number: Integer: 120, 
+// float, decimal: 3.65,
+
+// Math operations: +,-,*,/ 
+// shorthand: +=, -=, ++, --
+// convert; parseInt, parseFloat
+// toFixed

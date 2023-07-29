9b83f64827ac285785014507684e140bb5bd1911
devmashum <dev.mashum@gmail.com>
2023-07-29 19:58:33 +0200

    new commit

---
+++ push.pop.js
@@ -0,0 +1,24 @@
+// var numbers = [78, 45, 98, 45];
+// numbers.push(63);
+
+// numbers.pop(45);
+// // console.log(numbers);
+
+// numbers.pop(78);
+// // console.log(numbers);
+// numbers.pop(78);
+
+var numbers = [2, 3, 4, 5, 6];
+
+var newNumbers = 1;
+
+numbers.unshift(newNumbers);
+
+// // console.log(numbers);
+// newNumbers = removeFromBeginning(numbers)
+// // console.log(numbers);
+numbers.shift();
+
+console.log(numbers);
+
+

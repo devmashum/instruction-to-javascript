59cc198fc929f0ba9ec95902d18f79b6e99065c4
devmashum <dev.mashum@gmail.com>
2023-07-31 22:19:03 +0200

    new commit

---
+++ average.js
@@ -0,0 +1,20 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+console.log(totalMarks);
+
+var average = totalMarks / 5;
+
+console.log(average);
+
+var averageTwoDecimale = average.toFixed(2);
+
+var averageFourDecimale = average.toFixed(9);
+
+console.log(averageFourDecimale);
+

96005d60c28d7a7035f9b34ac18374f713b7c84c
devmashum <dev.mashum@gmail.com>
2023-07-31 22:07:08 +0200

    new commit

---
+++ apple-orange.js
@@ -0,0 +1,11 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalExpence = applePrice + orangePrice;
+
+console.log(totalExpence);
+
+var giveBack = moneyGiven - totalExpence;
+
+console.log(giveBack);

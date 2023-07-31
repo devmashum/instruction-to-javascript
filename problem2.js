05a8be189236b3907cc3416ad04f191ee1d28352
devmashum <dev.mashum@gmail.com>
2023-07-31 22:52:06 +0200

    new commit

---
+++ problem2.js
@@ -0,0 +1,6 @@
+var firstLine = ['I am going to be'];
+var secondLine = ['an awesome web developer'];
+
+
+var firstLine.push = [secondLine];
+
---
+++ problem4.js
@@ -0,0 +1,7 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+
+fruits.pop();
+
+fruits.push('Watermelon');
+console.log(fruits);

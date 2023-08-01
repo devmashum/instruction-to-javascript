b727ac564987f58198413dc4f9e12ab49430a833
devmashum <dev.mashum@gmail.com>
2023-08-01 22:26:33 +0200

    new commit

---
+++ loops.js
@@ -0,0 +1,6 @@
+var faisal = 1;
+
+while (faisal =< 10) {
+    console.log("i am sorry")
+    faisal++;
+}
---
+++ problem6.js
@@ -0,0 +1,10 @@
+var faisal = 1;
+
+while (faisal <= 10) {
+    console.log("i am sorry");
+    faisal++;
+}
+
+for (faisal = 1; faisal <= 10; faisal++) {
+    console.log("i am sorry", faisal)
+}

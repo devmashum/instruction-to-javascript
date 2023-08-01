a964996749f87baf2f090ffd3fc29bb04bff0d42
devmashum <dev.mashum@gmail.com>
2023-08-02 00:13:42 +0200

    new commit

--- for-loop.js
+++ for-loop.js
@@ -1,7 +1,15 @@
-for (i = 1; i <= 30; i++) {
-    if (i === 21) {
-        continue;
-    } console.log(i);
+// for (i = 1; i <= 30; i++) {
+//     if (i === 21) {
+//         continue;
+//     } console.log(i);
+// }
+
+var loveMeEla = true;
+var sexSatisfied = false;
+
+if (loveMeEla = true; && sexSatisfied == true; ) {
+    console.log('i will stay forever')
+
+} else {
+    console.log('i must leave forever')
 }
-
-
---
+++ problem7.js
@@ -0,0 +1,24 @@
+var loveMeEla = true;
+var sexSatisfied = false;
+var loveDaughter = true;
+
+if (loveMeEla == true || sexSatisfied == true || loveDaughter == true) {
+    console.log('i will stay forever')
+
+} else {
+    console.log('i must leave forever')
+}
+var totalPerson = 213;
+var bus = 50;
+var car = 11;
+
+
+var totalJatri = 590;
+var travelWithCar = 590 % 50;
+console.log('Travel with Bus:', 590 - travelWithCar)
+console.log('Travel with Car:', travelWithCar);
+var travelWithRikshaw = travelWithCar % 11;
+console.log('Travel with Rikshaw:', travelWithRikshaw);
+
+var rikshawKost = travelWithRikshaw * 70;
+console.log('Total cost of rikshaw:', rikshawKost);

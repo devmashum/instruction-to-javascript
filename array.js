7dfc3da1e746978b186a348575bd25a32efd2f32
devmashum <dev.mashum@gmail.com>
2023-08-01 12:16:34 +0200

    new commit

--- array.js
+++ array.js
@@ -1,1 +1,19 @@
 var friendsAge = [11, 21, 23, 23, 50, 30];
+// console.log(friendsAge.length);
+
+// console.log(friendsAge[4])
+
+// friendsAge.push(100)
+
+// // console.log(friendsAge)
+
+// friendsAge.pop()
+
+friendsAge.pop()
+
+console.log(friendsAge)
+friendsAge.shift()
+console.log(friendsAge)
+
+friendsAge.unshift(1000, 2000, 3000, 5000)
+console.log(friendsAge)

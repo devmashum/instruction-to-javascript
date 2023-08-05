c91dc55e3cada808cd490a15b20780537817ff89
devmashum <dev.mashum@gmail.com>
2023-08-05 20:58:00 +0200

    new commit

--- con2.js
+++ con2.js
@@ -1,11 +1,26 @@
-const name = "mehedi";
+// const name = "mehedi";
 
-const result = name.includes("li");
+// const result = name.includes("li");
 
-console.log(result)
+// console.log(result)
 
-const pets = ["cat", "dog", "rat", "Faisal"]
+// const pets = ["cat", "dog", "rat", "Faisal"]
 
-const rsult = pets.includes("Awlad");
+// const rsult = pets.includes("Awlad");
 
-console.log(rsult);
+// console.log(rsult);
+
+// const faisal = ["ant", "camel", "duck", "elephant", "cat", "dog",];
+
+// console.log(faisal.slice(2, 4));
+
+const Pets = ["Cat", "Tiger", "Dog", "Monkey", "Horst", "Panda"]
+
+// const result = Pets.slice(3, 6);
+
+// console.log(result);
+
+const result = Pets.splice(3, 3);
+
+console.log(result);
+
